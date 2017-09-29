c610a4f6c0acfda7bf233d01cc8767408bb81848
ciara hudson <cchudson33@gmail.com>
2017-09-29 01:17:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,11 @@
-var objects = {`playlist`};
+var playlist = new Object ({keys: '0'});
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistname) {
+  delete playlist[artistName]
+  return playlist
+}
